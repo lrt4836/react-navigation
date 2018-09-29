0ddb5e3ce20245bd53fd647a516e7e175e6bc28a
lrt4836 <296659110@qq.com>
2018-09-29 15:22:41 +0800

    1、修改iPhoneX的判断条件

--- src/views/SafeAreaView.js
+++ src/views/SafeAreaView.js
@@ -11,27 +11,25 @@ import {
 } from 'react-native';
 import withOrientation from './withOrientation';
 
-const { isIPhoneX_deprecated } = DeviceInfo;
 // See https://mydevice.io/devices/ for device dimensions
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
 const PAD_WIDTH = 768;
 const PAD_HEIGHT = 1024;
 
 const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window');
 
-const { PlatformConstants = {} } = NativeModules;
-const { minor = 0 } = PlatformConstants.reactNativeVersion || {};
-
 const isIPhoneX = (() => {
-  if (minor >= 50) {
-    return isIPhoneX_deprecated;
-  }
+  if (Platform.OS === 'web') return false;
 
   return (
     Platform.OS === 'ios' &&
     ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) ||
-      (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT))
+      (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT)) ||
+    ((D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) ||
+      (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT))
   );
 })();
 
